ae158e72e4fcce6482dff2773fb91d7a8556328f
Alyona <alyona.rodin@gmail.com>
2020-09-13 14:09:10 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 
 function App() {
   return (
     //BEM
     <div className="app">
-        <h1>Google</h1>
-        <Home />
-        {/*Search*/}
+      <Router>
+        <Switch>
+          <Route path='/search'>
+                <h1>Search page</h1>
+                {/* <SearchPage/> */}
+          </Route>
+          <Route path='/'>
+                <Home />
+          </Route>
+        </Switch> 
+      </Router>
     </div>
   );
 }
