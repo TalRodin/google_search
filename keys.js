c19ca5940df7d49b1e83a52acdc6de179fac3268
Alyona <alyona.rodin@gmail.com>
2020-09-14 12:11:01 -0400

    first commit

---
+++ keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyDVPRKuhD-zOgRKy4NQuCHLuCVypFKQkpY"
+
+export default API_KEY
---
+++ src/useGoogleSearch.js
@@ -0,0 +1,13 @@
+import {useState, useEffect} from 'react'
+import API_KEY from '../keys'
+
+const CONTEXT_KEY = '208356f13e8af5c40'
+
+const useGoogleSearch=()=> {
+    const [data, setData] = useState(null)
+    useEffect(()=>{
+        
+    }, [term])
+}
+
+export default useGoogleSearch
