b01d428b2bf788fc8312da84a6b12a91b375352c
Alyona <alyona.rodin@gmail.com>
2020-09-16 00:40:25 -0400

    first commit

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -1,13 +1,23 @@
 import {useState, useEffect} from 'react'
-import API_KEY from '../keys'
+import API_KEY from './keys'
 
 const CONTEXT_KEY = '208356f13e8af5c40'
 
-const useGoogleSearch=()=> {
+const useGoogleSearch=(term)=> {
     const [data, setData] = useState(null)
     useEffect(()=>{
-        
+        const fetchData= async()=>{
+            fetch(
+                `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+            )
+            .then(res=>res.json())
+            .then(result=>{
+                setData(result)
+            })
+        }
+        fetchData()
     }, [term])
+    return {data}
 }
 
 export default useGoogleSearch
